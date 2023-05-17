66c11b538c94c3427378d51f8994c967a56734f8
Segni Adeba <61460186+SegniAdebaGodsSon@users.noreply.github.com>
2023-05-17 15:31:41 +0300

    Prettier setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,18 @@
-import './globals.css'
-
+import './globals.css';
 
 export const metadata = {
   title: 'Datashelf - Landing page',
   description: 'This is the landing page of the Datashelf website',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
