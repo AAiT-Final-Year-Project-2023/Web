f50b865591357a2e24e3c123056bfdcce2e69e78
hanna kebede <se.hanna.kebede@gmail.com>
2023-05-25 14:33:29 +0300

    homepage

---
+++ src/app/home/page.tsx
@@ -0,0 +1,239 @@
+import styles from './page.module.scss'
+import Sidebar from "../sidebar/page"
+import Footer from "../footer/page"
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Data Shelf</title>
+      </head>
+      <div className="flex">
+      <div className='w-1/4 ml-3'><Sidebar/></div>
+      <div className='w-3/4 '>
+      <section className={styles.center}>
+          <div className={styles.container}>
+            <div>
+            {/* <div className='w-2/3'> */}
+          <form>   
+            <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+            <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                </div>
+                <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                
+            </div>
+          </form>
+          {/* </div> */}
+          {/* <div className='w-1/3'> */}
+          {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ml-40">
+                      B
+                    </span> */}
+          {/* </div> */}
+        </div>
+            
+              {/* <form role="search" className='mt-5 mb-6 ml-5'>
+                <label htmlFor="search">Search</label>
+                <input className="ml-5 " id="search" type="search" placeholder="Search..." autoFocus required />
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 ml-40">
+                      B
+                    </span>                
+              </form> */}
+              <h1 className="text-black-500 font-bold">Home</h1>
+              <h3 className='font-semibold'>Recently Viewed</h3>
+            
+              <ul className="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start p-8">
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-indigo-500">Title 1 UI</span>Completely unstyled, fully
+                accessible UI components</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind
+                    CSS.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://headlessui.dev">Learn
+                more<span className="sr-only">, Completely unstyled, fully accessible UI components</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/headlessui@75.c1d50bc1.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640" />
+    </li>
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-purple-500">Heroicons</span>Beautiful hand-crafted SVG
+                icons, by the makers of Tailwind CSS.</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>A set of 450+ free MIT-licensed SVG icons. Available as basic SVG icons and via first-party React and
+                    Vue libraries.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://heroicons.com">Learn
+                more<span className="sr-only">, Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/heroicons@75.4a558f35.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640"/>
+    </li>
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-cyan-500">Hero Patterns</span>Seamless SVG background
+                patterns by the makers of Tailwind CSS.</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>A collection of over 100 free MIT-licensed high-quality SVG patterns for you to use in your web
+                    projects.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://heropatterns.com">Learn
+                more<span className="sr-only">, Seamless SVG background patterns by the makers of Tailwind CSS.</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/heropatterns@75.82a09697.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640"/>
+    </li>
+</ul>
+
+              <h3 className='font-semibold'>Datasets</h3>
+              <ul className="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start p-8">
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-indigo-500">Headless UI</span>Completely unstyled, fully
+                accessible UI components</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind
+                    CSS.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://headlessui.dev">Learn
+                more<span className="sr-only">, Completely unstyled, fully accessible UI components</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/headlessui@75.c1d50bc1.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640" />
+    </li>
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-purple-500">Heroicons</span>Beautiful hand-crafted SVG
+                icons, by the makers of Tailwind CSS.</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>A set of 450+ free MIT-licensed SVG icons. Available as basic SVG icons and via first-party React and
+                    Vue libraries.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://heroicons.com">Learn
+                more<span className="sr-only">, Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/heroicons@75.4a558f35.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640"/>
+    </li>
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-cyan-500">Hero Patterns</span>Seamless SVG background
+                patterns by the makers of Tailwind CSS.</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>A collection of over 100 free MIT-licensed high-quality SVG patterns for you to use in your web
+                    projects.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://heropatterns.com">Learn
+                more<span className="sr-only">, Seamless SVG background patterns by the makers of Tailwind CSS.</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/heropatterns@75.82a09697.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640"/>
+    </li>
+</ul>
+
+              <h3 className='font-semibold'>Requests</h3>
+              <ul className="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start p-8">
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-indigo-500">Headless UI</span>Completely unstyled, fully
+                accessible UI components</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>Completely unstyled, fully accessible UI components, designed to integrate beautifully with Tailwind
+                    CSS.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://headlessui.dev">Learn
+                more<span className="sr-only">, Completely unstyled, fully accessible UI components</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/headlessui@75.c1d50bc1.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640" />
+    </li>
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-purple-500">Heroicons</span>Beautiful hand-crafted SVG
+                icons, by the makers of Tailwind CSS.</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>A set of 450+ free MIT-licensed SVG icons. Available as basic SVG icons and via first-party React and
+                    Vue libraries.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://heroicons.com">Learn
+                more<span className="sr-only">, Beautiful hand-crafted SVG icons, by the makers of Tailwind CSS.</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/heroicons@75.4a558f35.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640"/>
+    </li>
+    <li className="relative flex flex-col sm:flex-row xl:flex-col items-start">
+        <div className="order-1 sm:ml-6 xl:ml-0">
+            <h3 className="mb-1 text-slate-900 font-semibold dark:text-slate-200">
+                <span className="mb-1 block text-sm leading-6 text-cyan-500">Hero Patterns</span>Seamless SVG background
+                patterns by the makers of Tailwind CSS.</h3>
+            <div className="prose prose-slate prose-sm text-slate-600 dark:prose-dark">
+                <p>A collection of over 100 free MIT-licensed high-quality SVG patterns for you to use in your web
+                    projects.</p>
+            </div><a
+                className="group inline-flex items-center h-9 rounded-full text-sm font-semibold whitespace-nowrap px-3 focus:outline-none focus:ring-2 bg-slate-100 text-slate-700 hover:bg-slate-200 hover:text-slate-900 focus:ring-slate-500 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600 dark:hover:text-white dark:focus:ring-slate-500 mt-6"
+                href="https://heropatterns.com">Learn
+                more<span className="sr-only">, Seamless SVG background patterns by the makers of Tailwind CSS.</span><svg
+                    className="overflow-visible ml-3 text-slate-300 group-hover:text-slate-400 dark:text-slate-500 dark:group-hover:text-slate-400"
+                    width="3" height="6" viewBox="0 0 3 6" fill="none" stroke="currentColor" stroke-width="2"
+                    stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M0 0L3 3L0 6"></path>
+                </svg></a>
+        </div><img src="https://tailwindcss.com/_next/static/media/heropatterns@75.82a09697.jpg" alt="" className="mb-6 shadow-md rounded-lg bg-slate-50 w-full sm:w-[17rem] sm:mb-0 xl:mb-6 xl:w-full" width="1216" height="640"/>
+    </li>
+</ul>
+
+
+
+            </div>
+          </section>
+      
+      </div>
+      </div> 
+      <Footer/>
+    </div>
+  )
+}
