a5ff5a1380ace43ab636e1ab6c9c7f9f209e8b12
hanna kebede <se.hanna.kebede@gmail.com>
2023-05-28 05:51:15 +0300

    requestpage

--- src/app/datasets/page.tsx
+++ src/app/datasets/page.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "../sidebar/page"
 import Footer from "../footer/page"
+import Search from "../navsearchprofile/page"
 
 export default function Dataset() {
   return (
@@ -13,17 +14,8 @@ export default function Dataset() {
       <div className='w-3/4'>
       <section className="">
           <div className="">
-              <form>
-              <div className="relative">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                    <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                </div>
-                <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
-                <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-                
-            </div>
-          </form>
-              <h1 className="text-black-600 ml-4">Datasets</h1>
+              <Search/>
+              <h1 className="font-bold text-xl mt-3 ml-4">Datasets</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo</p>
               <input id="search" type="search" placeholder="Search Datasets" autoFocus required />
@@ -46,7 +38,7 @@ export default function Dataset() {
     </ul>
 </div>
 
-              <h3>Recently Viewed</h3>
+              <h3 className="font-bold ml-2">Recently Viewed</h3>
             
             <div className="flex flex-col md:flex-row">
               <div className='flex w-full flex-nowrap'>
@@ -233,6 +225,57 @@ export default function Dataset() {
 
               </div>
               </div>
+    
+
+              <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    
+    <div className="rounded overflow-hidden shadow-lg">
+      <img className="w-full" src="https://source.unsplash.com/random/1600x900" alt="Mountain"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Mountain</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+      </div>
+    </div>
+    
+    <div className="rounded overflow-hidden shadow-lg">
+      <img className="w-full" src="https://source.unsplash.com/random/1600x900" alt="River"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">River</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#summer</span>
+      </div>
+    </div>
+
+    
+    <div className="rounded overflow-hidden shadow-lg">
+      <img className="w-full" src="https://source.unsplash.com/random/1600x900" alt="Forest"/>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">Forest</div>
+        <p className="text-gray-700 text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, Nonea! Maiores et perferendis eaque, exercitationem praesentium nihil.
+        </p>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#fall</span>
+      </div>
+    </div>
+  </div>
+    
 
 
             </div>
---
+++ src/app/requests/page.tsx
@@ -0,0 +1,98 @@
+import Sidebar from "../sidebar/page"
+import Footer from "../footer/page"
+import Search from "../navsearchprofile/page"
+
+export default function Requests() {
+  return (
+    <div>
+      <head>
+        <title>Data Shelf</title>
+      </head>
+      <div className="flex w-full">
+      <div className='w-1/4 ml-3'><Sidebar/></div>
+      <div className='w-3/4'>
+      <section className="">
+          <div className="">
+          <Search/>
+              <h1 className="font-bold text-xl ml-2">Requests</h1>
+              <p className="mb-4">loream episom loream episomloream episom loream episomloream episom loream episomloream episom loream episomloream episom loream episomloream episom loream episomloream episom loream episomloream episom loream episomloream episom loream 
+    episom loream episomloream episom loream episom</p>
+
+
+<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-3">
+    <div className="md:flex">
+        <div className="md:shrink-0">
+            <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnjiLxl1lHrBI3-W4QqXz8RqkQmSTbcOBrBQ&usqp=CAU" />
+        </div>
+        <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Flowers</div>
+            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible
+                accommodation for your team
+            </a>
+            <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in
+                some sunshine? We have a list of places to do just that.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-3">
+    <div className="md:flex">
+        <div className="md:shrink-0">
+            <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://loremflickr.com/g/320/240/team" />
+        </div>
+        <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible
+                accommodation for your team
+            </a>
+            <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in
+                some sunshine? We have a list of places to do just that.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-3">
+    <div className="md:flex">
+        <div className="md:shrink-0">
+            <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTBWsNFeXpY3KPGzX350RaxfqfuiS29tnLXA&usqp=CAU" />
+        </div>
+        <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible
+                accommodation for your team
+            </a>
+            <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in
+                some sunshine? We have a list of places to do just that.
+            </p>
+        </div>
+    </div>
+</div>
+
+<div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-3">
+    <div className="md:flex">
+        <div className="md:shrink-0">
+            <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://loremflickr.com/g/320/240/team" />
+        </div>
+        <div className="p-8">
+            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible
+                accommodation for your team
+            </a>
+            <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in
+                some sunshine? We have a list of places to do just that.
+            </p>
+        </div>
+    </div>
+</div>
+
+            </div>
+          </section>
+      
+      </div>
+      </div> 
+      <Footer/>
+    </div>
+  )
+}
