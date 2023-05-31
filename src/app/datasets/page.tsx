0e7d2c7af9e3d77801a2e58e1a7aa8482ae37e6c
hanna kebede <se.hanna.kebede@gmail.com>
2023-05-31 15:43:47 +0300

    edits

--- src/app/datasets/page.tsx
+++ src/app/datasets/page.tsx
@@ -1,6 +1,7 @@
 import Sidebar from "../sidebar/page"
 import Footer from "../footer/page"
 import Search from "../navsearchprofile/page"
+import Filter from "../searchsort/page"
 
 export default function Dataset() {
   return (
@@ -18,27 +19,10 @@ export default function Dataset() {
               <h1 className="font-bold text-4xl mt-3 ml-4">Datasets</h1>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolo</p>
-              <input id="search" type="search" placeholder="Search Datasets" autoFocus required />
-              
-<button id="dropdownDelayButton" data-dropdown-toggle="dropdownDelay" data-dropdown-delay="500" data-dropdown-trigger="hover" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown hover <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-<div id="dropdownDelay" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDelayButton">
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-      </li>
-      <li>
-        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-      </li>
-    </ul>
-</div>
+                
+              <Filter/>
 
-              <h3 className="font-bold ml-2">Recently Viewed</h3>
+              <h3 className="font-bold text-3xl ml-2 mb-4">Recently Viewed</h3>
             
             <div className="flex flex-col md:flex-row">
               <div className='flex w-full flex-nowrap'>
@@ -46,17 +30,9 @@ export default function Dataset() {
                 <div className="max-w-sm rounded overflow-hidden shadow-lg">
                 <img className="w-full" src="https://source.unsplash.com/random/1600x900" alt="Sunset in the mountains" />
                   <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p className="text-gray-700 text-base">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                  </div>
-                  <div className="px-6 py-4">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                      #photography
-                    </span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+                    <div className="font-bold text-2xl mb-2">The Coldest Sunset</div>
+                    <h1 className="text-xl font-semibold mb-2">Jhon David</h1>
+                    <p className="text-gray-700 text-base">JSON Dataset | 1k datas</p>
                   </div>
                 </div>
                 </div>
@@ -65,36 +41,20 @@ export default function Dataset() {
                 <div className="max-w-sm rounded overflow-hidden shadow-lg">
                 <img className="w-full" src="https://source.unsplash.com/random/1600x900" alt="Sunset in the mountains" />
                   <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p className="text-gray-700 text-base">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                  </div>
-                  <div className="px-6 py-4">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                      #photography
-                    </span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+                    <div className="font-bold text-2xl mb-2">The Coldest Sunset</div>
+                    <h1 className="text-xl font-semibold mb-2">Jhon David</h1>
+                    <p className="text-gray-700 text-base">JSON Dataset | 1k datas</p>
                   </div>
                 </div>
                 </div>
 
-                <div className="w-full grow items-center h-screen">
+                <div className="w-full grow items-center h-screen mr-4">
                 <div className="max-w-sm rounded overflow-hidden shadow-lg">
                 <img className="w-full" src="https://source.unsplash.com/random/1600x900" alt="Sunset in the mountains" />
                   <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p className="text-gray-700 text-base">
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                  </div>
-                  <div className="px-6 py-4">
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                      #photography
-                    </span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+                    <div className="font-bold text-2xl mb-2">The Coldest Sunset</div>
+                    <h1 className="text-xl font-semibold mb-2">Jhon David</h1>
+                    <p className="text-gray-700 text-base">JSON Dataset | 1k datas</p>
                   </div>
                 </div>
                 </div>
