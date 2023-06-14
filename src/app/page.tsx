308a85b75a389a7aed984daaadbef7c0b7aab9e0
hanna kebede <se.hanna.kebede@gmail.com>
2023-06-14 14:08:18 +0300

    edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,7 @@ export default function Home() {
       </div>
 
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
-
-        {/* Hero content */}
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-
-          {/* Section header */}
           <div className="text-center pb-12 md:pb-16">
             <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">Unlock the Power of  <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">Data Collection</span></h1>
             <div className="max-w-3xl mx-auto">
@@ -21,10 +17,7 @@ export default function Home() {
                 This is a platform for public data sharing, requests, and contributions. cooperating to collect data while being compensated for your contribution</p>
               <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                 <div>
-                  <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-                </div>
-                <div>
-                  <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
+                  <a className="btn text-white bg-blue-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="/login">Learn more</a>
                 </div>
               </div>
             </div>
@@ -34,16 +27,11 @@ export default function Home() {
 
         <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="py-12 md:py-20">
-
-          {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
             <h2 className="h2 mb-4 font-bold text-4xl">Trusted by over 20,000 companies all over the world</h2>
           </div>
-
-          {/* Items */}
           <div className="max-w-sm md:max-w-4xl mx-auto grid gap-2 grid-cols-4 md:grid-cols-5">
 
-            {/* Item */}
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
                 <h1 className="h2 mb-4 font-bold text-xl text-gray-400">Request for New Data Collection</h1>
             </div>
@@ -61,16 +49,14 @@ export default function Home() {
             </div>
 
             <div className="flex items-center justify-center py-2 col-span-2 md:col-auto">
-                <h1 className="h2 mb-4 font-bold text-xl text-gray-400"></h1>
+                <h1 className="h2 mb-4 font-bold text-xl text-gray-400">Get labled Datas as your Request</h1>
             </div>
 
           </div>
 
-          {/* Testimonials */}
           <div className="max-w-3xl mx-auto mt-20" data-aos="zoom-y-out">
             <div className="relative flex items-start border-2 border-gray-200 rounded bg-white">
 
-              {/* Testimonial */}
               <div className="text-center px-12 py-8 pt-20 mx-4 md:mx-0">
                 <div className="absolute top-0 -mt-8 left-1/2 transform -translate-x-1/2">
                   <svg className="absolute top-0 right-0 -mt-3 -mr-8 w-16 h-16 fill-current text-blue-500" viewBox="0 0 64 64" aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
@@ -78,11 +64,11 @@ export default function Home() {
                   </svg>
                 </div>
                 <blockquote className="text-xl font-medium mb-4">
-                  “ I love this product and would recommend it to anyone. Could be not easier to use, and our multiple websites are wonderful. We get nice comments all the time. “
+                  “ I love this platform and would recommend it to anyone. Could be not easier to use, can access and require dataset based on what you needed. And help others to get datasets prepared already for any purpose. We get nice comments all the time. “
                 </blockquote>
                 <cite className="block font-bold text-lg not-italic mb-1">Darya Finger</cite>
                 <div className="text-gray-600">
-                  <span>CEO & Co-Founder</span> <a className="text-blue-600 hover:underline" href="#0">@Dropbox</a>
+                  <span></span>The highest dataset owner <a className="text-blue-600 hover:underline" href="#0"></a>
                 </div>
               </div>
 
@@ -94,7 +80,8 @@ export default function Home() {
 
       </div>
 
-      <Footer/>
+<div><Footer/></div>
+      
     </div>
 
   )}
